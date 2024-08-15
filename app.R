6b7e48b8a74991a08ab7b7c51e502ccfab77ba33
cdyzh <cindyzhangty@hotmail.com>
2024-08-15 08:48:55 -0400

    added new upload option and PDF download

--- app.R
+++ app.R
@@ -2,6 +2,9 @@ library(shiny)
 library(dplyr)
 library(yogiroc)
 library(rsconnect)
+library(rmarkdown)
+library(knitr)
+library(tinytex)
 
 # Define UI for the application
 ui <- fluidPage(
@@ -9,12 +12,19 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      checkboxInput("upload_file", "Upload Your Own Reference Set", value = FALSE),
+      actionButton("upload_guide", "Upload Dataset Guide", class = "btn-primary"),
+      selectInput("upload_type", "Select Upload Option:",
+                  choices = list("Full Dataset (All Columns)" = "full", "Gene and Variant ID Only" = "gene_variant"),
+                  selected = "full"),
       conditionalPanel(
-        condition = "input.upload_file == true",
-        fileInput("file", "Upload CSV File", 
-                  accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")), 
-                  actionButton("infoButton", "Formatting Guide", class = "btn-info")
+        condition = "input.upload_type == 'full'",
+        fileInput("file_full", "Upload Full CSV File", 
+                  accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv"))
+      ),
+      conditionalPanel(
+        condition = "input.upload_type == 'gene_variant'",
+        fileInput("file_gene_variant", "Upload Gene and Variant ID CSV File", 
+                  accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv"))
       ),
       selectizeInput("gene", "Select Gene Name:", choices = NULL, options = list(maxOptions = 1000)),
       checkboxInput("common_variant_filter", "Exclude Common Variants (gnomAD AF > 0.005)", value = TRUE),
@@ -24,12 +34,14 @@ ui <- fluidPage(
                            "REVEL",
                            "AlphaMissense"
                          ),
-                         selected = c("VARITY", "REVEL", "AlphaMissense") # Check all by default
+                         selected = c("VARITY", "REVEL", "AlphaMissense")
       ),
       actionButton("plotButton", "Generate PRC Plot"),
-      downloadButton("downloadPlot", "Download PRC Plot"),
+      downloadButton("downloadPlotPNG", "Download PRC Plot as PNG"),
+      downloadButton("downloadPlotPDF", "Download PRC Plot and Metadata"),
       helpText("Example genes: LDLR, TTN (no VARITY), ACADVL, DNM2, MYH7, SCN5A, etc."),
-      helpText("If error occurs, it means there is not enough data to generate the PRC. Try using fewer predictors.")
+      helpText("If error occurs, it means there is not enough data to generate the PRC. Try using fewer predictors."), 
+      actionButton("helpButton", "Help (Q&A)", class = "btn-warning")
     ),
     
     mainPanel(
@@ -42,7 +54,6 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   plot_data <- reactiveVal(NULL)
   
-  # Replace column names with standard names
   standard_colnames <- c(
     "base__hugo", 
     "gnomad__af", 
@@ -52,20 +63,29 @@ server <- function(input, output, session) {
     "classification"
   )
   
-  # Read inputted csv, else read existing csv
   prcdata <- reactive({
-    if (input$upload_file) {
-      req(input$file)
-      df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
-      # Automatically rename columns to match the standard column names
+    if (input$upload_type == "full" && !is.null(input$file_full)) {
+      req(input$file_full)
+      df <- read.csv(input$file_full$datapath, stringsAsFactors = FALSE)
       colnames(df) <- standard_colnames
       df
+    } else if (input$upload_type == "gene_variant" && !is.null(input$file_gene_variant)) {
+      req(input$file_gene_variant)
+      df <- read.csv(input$file_gene_variant$datapath, stringsAsFactors = FALSE)
+      
+      # Load the full stored dataset for matching
+      full_df <- read.csv("preprocessed_id.csv", stringsAsFactors = FALSE)
+      colnames(df) <- c("base__hugo", "base__achange")
+      
+      # Merge based on gene and variant ID
+      df <- merge(df, full_df, by = c("base__hugo", "base__achange"))
+      print(df)
+      df
     } else {
-      read.table("preprocessed_full.csv", sep = ',', header = TRUE, stringsAsFactors = FALSE) # 175166 rows after processing
+      read.table("preprocessed_id.csv", sep = ',', header = TRUE, stringsAsFactors = FALSE)
     }
   })
   
-  # Update the dropdown menu with gene names
   observe({
     df <- prcdata()
     if (!is.null(df)) {
@@ -74,7 +94,6 @@ server <- function(input, output, session) {
     }
   })
   
-  # Update which scores exist based on selected gene
   observe({
     req(input$gene)
     df <- prcdata()
@@ -94,7 +113,6 @@ server <- function(input, output, session) {
     updateCheckboxGroupInput(session, "scores", choices = available_scores, selected = available_scores)
   })
   
-  # Plot
   observeEvent(input$plotButton, {
     df <- prcdata()
     gene_s <- input$gene
@@ -104,36 +122,41 @@ server <- function(input, output, session) {
     names(df)[names(df) == "varity_r__varity_r"] <- "VARITY"
     names(df)[names(df) == "alphamissense__am_pathogenicity"] <- "AlphaMissense"
     names(df)[names(df) == "revel__score"] <- "REVEL"
+    names(df)[names(df) == "gnomad__af"] <- "gnomAD_AF"
     
-    # Common variant filter
     if (exclude_common_variants) {
-      df <- df[is.na(df$gnomad__af) | df$gnomad__af <= 0.005, ]
+      df <- df[is.na(df$gnomAD_AF) | df$gnomAD_AF <= 0.005, ]
     }
     
-    # Ordering is needed for yrobj to work
     df <- df[order(df$classification), ]
     
-    # Gene filter
     prcfiltered <- df %>%
       filter(base__hugo == gene_s)
     
     B_org <- sum(prcfiltered$classification == TRUE & rowSums(!is.na(prcfiltered[selected_scores])) > 0)
     P_org <- sum(prcfiltered$classification == FALSE & rowSums(!is.na(prcfiltered[selected_scores])) > 0)
     
-    # Generate the yrobj object
-    yrobj <- yr2(truth = prcfiltered[["classification"]], scores = prcfiltered[selected_scores], high = rep(FALSE, length(selected_scores)))
-
-    plot_data(list(
-      yrobj = yrobj,
-      lty_styles = c("dashed", "solid", "dashed")[1:length(selected_scores)],
-      col_styles = c("purple", "cadetblue2", "orange")[1:length(selected_scores)],
-      gene_s = gene_s,
-      selected_scores = selected_scores,
-      B_org = B_org,
-      P_org = P_org
-    ))
+    tryCatch({
+      yrobj <- yr2(truth = prcfiltered[["classification"]], scores = prcfiltered[selected_scores], high = rep(FALSE, length(selected_scores)))
+      
+      plot_data(list(
+        yrobj = yrobj,
+        lty_styles = c("dashed", "solid", "dashed")[1:length(selected_scores)],
+        col_styles = c("purple", "cadetblue2", "orange")[1:length(selected_scores)],
+        gene_s = gene_s,
+        selected_scores = selected_scores,
+        B_org = B_org,
+        P_org = P_org,
+        prcfiltered = prcfiltered  # Save the filtered data for metadata
+      ))
+      
+      output$errorText <- renderText("")
+      
+    }, error = function(e) {
+      plot_data(NULL)
+      output$errorText <- renderText("Not enough data")
+    })
     
-    # Attempt to generate plot, catch errors
     output$prcPlot <- renderPlot({
       plot_info <- plot_data()
       if (!is.null(plot_info)) {
@@ -153,52 +176,7 @@ server <- function(input, output, session) {
     }, width = 600, height = 600, res = 72)
   })
   
-  # Guide on how to format user-inputted csv
-  observeEvent(input$infoButton, {
-    showModal(modalDialog(
-      title = "Reference Set Format Information",
-      HTML("Please ensure your own reference set is a CSV file and is formatted with columns in the following order:<br><br>
-            <b>gene:</b> Gene names<br>
-            <b>gnomad_af:</b> GnomAD allele frequency<br>
-            <b>varity:</b> VARITY score<br>
-            <b>alphamissense:</b> AlphaMissense score<br>
-            <b>revel:</b> REVEL score<br>
-            <b>classification:</b> Variant classification (TRUE for Pathogenic, FALSE for Benign)<br><br>
-            Please make sure to keep all columns. You can put NA for gnomad if you do not need the common variant filter, and put NA for each predictor if you do not wish to include it. <br><br>
-            <b>Example Table:</b><br>
-            <table border='1'>
-              <tr>
-                <th>gene</th>
-                <th>gnomad_af</th>
-                <th>varity</th>
-                <th>alphamissense</th>
-                <th>revel</th>
-                <th>classification</th>
-              </tr>
-              <tr>
-                <td>LDLR</td>
-                <td>NA</td>
-                <td>0.85</td>
-                <td>0.95</td>
-                <td>NA</td>
-                <td>TRUE</td>
-              </tr>
-              <tr>
-                <td>LDLR</td>
-                <td>NA</td>
-                <td>0.65</td>
-                <td>0.75</td>
-                <td>NA</td>
-                <td>FALSE</td>
-              </tr>
-            </table>"),
-      easyClose = TRUE,
-      footer = NULL
-    ))
-  })
-
-  # Download plot
-  output$downloadPlot <- downloadHandler(
+  output$downloadPlotPNG <- downloadHandler(
     filename = function() {
       paste("PRC_plot_", input$gene, ".png", sep = "")
     },
@@ -213,6 +191,64 @@ server <- function(input, output, session) {
       }
     }
   )
+  
+  output$downloadPlotPDF <- downloadHandler(
+    filename = function() {
+      paste("PRC_Report_", input$gene, ".pdf", sep = "")
+    },
+    content = function(file) {
+      plot_info <- plot_data()
+      if (!is.null(plot_info)) {
+        # Generate the PDF report
+        rmarkdown::render(input = "report_template.Rmd",
+                          output_file = file,
+                          params = list(
+                            gene_s = plot_info$gene_s,
+                            selected_scores = plot_info$selected_scores,
+                            B_org = plot_info$B_org,
+                            P_org = plot_info$P_org,
+                            prcfiltered = plot_info$prcfiltered
+                          ),
+                          envir = new.env(parent = globalenv()))
+      }
+    }
+  )
+  
+  observeEvent(input$upload_guide, {
+    showModal(modalDialog(
+      title = "Upload Dataset Guide",
+      HTML("<h4>Option 1: Full Dataset (All Columns)</h4>
+            <p>Upload a CSV file with the following columns:</p>
+            <ul>
+              <li><b>gene:</b> Gene name</li>
+              <li><b>gnomad_af:</b> gnomAD allele frequency</li>
+              <li><b>varity_r:</b> VARITY score</li>
+              <li><b>alphamissense:</b> AlphaMissense score</li>
+              <li><b>revel:</b> REVEL score</li>
+              <li><b>classification:</b> TRUE for Pathogenic, FALSE for Benign</li>
+            </ul>
+            <h4>Option 2: Gene and Variant ID Only</h4>
+            <p>Upload a CSV file with the following columns:</p>
+            <ul>
+              <li><b>gene:</b> Gene name</li>
+              <li><b>variant_id:</b> Variant ID</li>
+            </ul>
+            <p>The data will be matched with the stored dataset to retrieve corresponding scores.</p>"),
+      easyClose = TRUE,
+      footer = modalButton("Close")
+    ))
+  })
+  
+  observeEvent(input$helpButton, {
+    showModal(modalDialog(
+      title = "Q&A",
+      HTML("<p><b>What is balanced precision?</b></p>
+            <p>Balanced precision is useful in situations where the class distribution is imbalanced. In other words, it is the precision that would have been expected had the proportion of positive examples been balanced (equal to 50%).</p>
+           <p>Definition at https://doi.org/10.1016/j.ajhg.2021.08.012</p>"),
+      easyClose = TRUE,
+      footer = modalButton("Close")
+    ))
+  })
 }
 
 # Run the application
